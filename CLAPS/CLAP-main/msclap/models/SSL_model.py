58d56b654cd1145c1695e81731fccb2bda85866f
Anshuman Sinha <101933449+anshumansinha16@users.noreply.github.com>
2023-10-16 11:37:55 -0400

    SSL update transformer included

--- CLAPS/CLAP-main/msclap/models/SSL_model.py
+++ CLAPS/CLAP-main/msclap/models/SSL_model.py
@@ -9,70 +9,72 @@ from msclap import CLAP
 
 import yaml
 
+from torch.utils.data import Dataset
+from torchvision.datasets.utils import download_url
+from tqdm import tqdm
+import pandas as pd
+import os
+import torch.nn as nn
+import torch
+from datasets import load_dataset
 
-def load_config(file_path):
-    with open(file_path, 'r') as stream:
-        try:
-            return yaml.safe_load(stream)
-        except yaml.YAMLError as exc:
-            print(exc)
-            return None
 
+class MultiModalAttention(nn.Module):
+    def __init__(self, audio_embed_dim, text_embed_dim, attention_dim):
+        super(MultiModalAttention, self).__init__()
+        self.audio_attention = nn.Linear(audio_embed_dim, attention_dim)
+        self.text_attention = nn.Linear(text_embed_dim, attention_dim)
+        self.combined_attention = nn.Linear(attention_dim, 1)
+
+    def forward(self, audio_embed, text_embed):
+        audio_attn = self.audio_attention(audio_embed)
+        text_attn = self.text_attention(text_embed)
+        
+        # Combine the attention weights
+        combined = torch.tanh(audio_attn + text_attn)
+        attention_weights = F.softmax(self.combined_attention(combined), dim=1)
+
+        return attention_weights
 
 class ExtendedModel(nn.Module):
     def __init__(self, base_model):
         super(ExtendedModel, self).__init__()
-        self.base = base_model
+
+        self.audio_encoder = base_model.audio_encoder
+        self.text_encoder = base_model.caption_encoder
+
+        text_embed_dim = self.text_encoder.projection.linear2.out_features
+        audio_embed_dim = self.audio_encoder.projection.linear2.out_features
         
-        # Add new layers herec
-        self.new_layer = nn.Linear(in_features=10, out_features=5) 
-        
-    def forward(self, x):
-        x = self.base(x)
-        x = self.new_layer(x)
-        return x
+        # Attention mechanism
+        attention_dim = 128  
+        self.attention = MultiModalAttention(audio_embed_dim, text_embed_dim, attention_dim)
 
+        # Deep transformation network
+        hidden_dim = 512 
+        self.transformation = nn.Sequential(
+            nn.Linear(audio_embed_dim + text_embed_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Dropout(0.5),  # regularization
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Dropout(0.5)  # regularization
+        )
 
-if __name__ == "__main__":
-    # Load your configuration file
-    config_path = '../configs/config_2022.yml'  # Replace with your actual file path
-    config = load_config(config_path)
+        # Task-specific layer: Adjust depending on your task
+        self.task_layer = nn.Linear(hidden_dim, 5)  # Assuming a task-specific size
 
-    # Load and initialize CLAP
-    weights_path = '/Users/anshumansinha/Desktop/Fall23/CSE8803/Project/CLAP_weights_2022.pth'
-    clap_model = CLAP(weights_path, version = '2022', use_cuda=False)
+    def forward(self, audio, text):
+        audio_embed = self.audio_encoder(audio)
+        text_embed = self.text_encoder(text)
 
+        attention_weights = self.attention(audio_embed, text_embed)
 
-    clap_instance = CLAPS(
-                audioenc_name=config['audioenc_name'],
-                sample_rate=config['sampling_rate'],  # 'sample_rate' seems to be the correct parameter name
-                window_size=config['window_size'],
-                hop_size=config['hop_size'],
-                mel_bins=config['mel_bins'],
-                fmin=config['fmin'],
-                fmax=config['fmax'],
-                classes_num=config['num_classes'],  # It seems 'classes_num' is the correct parameter name
-                out_emb=config['out_emb'],
-                text_model=config['text_model'],
-                transformer_embed_dim=config['transformer_embed_dim'],
-                d_proj=config['d_proj']
-            )
+        # Weighted combination of features
+        combined = attention_weights * audio_embed + attention_weights * text_embed
+        # Deep transformation
+        transformed = self.transformation(combined)
+        # Task-specific layer
+        output = self.task_layer(transformed)
 
-    state_dict = torch.load('/Users/anshumansinha/Desktop/Fall23/CSE8803/Project/CLAP_weights_2022.pth', map_location=torch.device('cpu'))
-
-    # Assuming you have loaded your checkpoint as follows
-    checkpoint = torch.load('/Users/anshumansinha/Desktop/Fall23/CSE8803/Project/CLAP_weights_2022.pth', map_location=torch.device('cpu'))
-
-    # Extract the state dict for the model
-    model_state_dict = checkpoint['model']  # 'model' here refers to the key containing the model's state_dict
-    clap_instance.load_state_dict(state_dict,strict=False)
-
-    extended_model = ExtendedModel(clap_instance)
-
-    #def forward(self, audio, text): # check
-
-    x = torch.rand(3, 4)
-
-    out = extended_model(x)
-    exit()
-
+        return output
